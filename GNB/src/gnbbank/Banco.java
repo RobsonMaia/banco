42091fb325043d9f4e627aacb856a1da739c72ad
RobsonMaia <robsonmaia53@gmail.com>
2022-07-06 12:16:51 -0300

    primeiro Teste de modificação

--- GNB/src/gnbbank/Banco.java
+++ GNB/src/gnbbank/Banco.java
@@ -10,46 +10,39 @@ public class Banco {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		
 		Conta conta1 = new Conta();
 		conta1.agencia = 12345;
 		conta1.numConta = 123;
 		conta1.saldo = 500;
 		conta1.titular = "Robson Maia";
+		System.out.println("-----------------");
+		System.out.println("      CONTA      ");
+		System.out.println("-----------------");
+		System.out.println("   ");
 		System.out.println(conta1);
-		System.out.print("Valor do deposito: R$");
-		double valor = sc.nextDouble();
-		conta1.dopositar(valor);
-		System.out.println("   ");
-		System.out.println("CONTA ATUALIZADA: " + conta1);
+		System.out.println("Qual opeeração deseja realizar?");
+		System.out.println("[1] SAQUE");
+		System.out.println("[2] DEPOSITO");
+		int cont = sc.nextInt();
+		switch (cont) {
+		case 1:
+			System.out.print(conta1.titular + " Valor do saque: R$");
+			double saque = sc.nextDouble();
+			conta1.sacar(saque);
+			System.out.println("   ");
+			System.out.println("CONTA ATUALIZADA: " + conta1);
+		break;
+		case 2:
+			System.out.print("Valor do deposito: R$");
+			double valor = sc.nextDouble();
+			conta1.dopositar(valor);
+			System.out.println("   ");
+			System.out.println("CONTA ATUALIZADA: " + conta1);
+		break;	
+		}
 		
-		System.out.println("   ");
-		Conta conta2 = new Conta();
-		conta2.agencia = 54321;
-		conta2.numConta = 321;
-		conta2.saldo = 3000;
-		conta2.titular = "Gabriela Barbosa";
-		System.out.println(conta2);
-		System.out.print("Valor do deposito: R$");
-		double valor2 = sc.nextDouble();
-		conta2.dopositar(valor2);
-		System.out.println("   ");
-		System.out.println("CONTA ATUALIZADA: " + conta2);
-		
-		System.out.println("   ");
-		System.out.print(conta1.titular + " Valor do saque: R$");
-		double saque = sc.nextDouble();
-		conta1.sacar(saque);
-		System.out.println("   ");
-		System.out.println("CONTA ATUALIZADA: " + conta1);
-		
-		System.out.println("   ");
-		System.out.print(conta2.titular + " Valor do saque: R$");
-		double saque1 = sc.nextDouble();
-		conta2.sacar(saque1);
-		System.out.println("   ");
-		System.out.println("CONTA ATUALIZADA: " + conta2);
-		
-
+			
 		sc.close();
 	}
 
