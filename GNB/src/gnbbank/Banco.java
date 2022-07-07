ddae537a44ebbd7582d4109cb402c8685d90af82
RobsonMaia <robsonmaia53@gmail.com>
2022-07-07 12:04:07 -0300

    inclusão de senha

--- GNB/src/gnbbank/Banco.java
+++ GNB/src/gnbbank/Banco.java
@@ -10,6 +10,7 @@ public class Banco {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		int senha;
 		
 		Conta conta1 = new Conta();
 		conta1.agencia = 12345;
@@ -20,13 +21,20 @@ public class Banco {
 		System.out.println("      CONTA      ");
 		System.out.println("-----------------");
 		System.out.println("   ");
-		System.out.println(conta1);
+		System.out.println("TITULAR: " + conta1.titular);
 		System.out.println("Qual opeeração deseja realizar?");
 		System.out.println("[1] SAQUE");
 		System.out.println("[2] DEPOSITO");
 		int cont = sc.nextInt();
 		switch (cont) {
 		case 1:
+			System.out.println("INFORME A SENHA:");
+			senha = sc.nextInt();
+			while (senha != 2022) {
+				System.out.println("SENHA INVALIDA!");
+				senha = sc.nextInt();	
+			}
+			System.out.println("ACESSO PERMITIDO :)");
 			System.out.print(conta1.titular + " Valor do saque: R$");
 			double saque = sc.nextDouble();
 			conta1.sacar(saque);
@@ -34,6 +42,13 @@ public class Banco {
 			System.out.println("CONTA ATUALIZADA: " + conta1);
 		break;
 		case 2:
+			System.out.println("INFORME A SENHA:");
+			senha = sc.nextInt();
+			while (senha != 2022) {
+				System.out.println("SENHA INVALIDA!");
+				senha = sc.nextInt();	
+			}
+			System.out.println("ACESSO PERMITIDO :)");
 			System.out.print("Valor do deposito: R$");
 			double valor = sc.nextDouble();
 			conta1.dopositar(valor);
